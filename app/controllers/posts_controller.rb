71d6bbb00682cbb7cb924b218095f36d3ba26d5b
sanlouise <sandra.adamshallie@gmail.com>
2017-05-21 09:52:56 -0400

    Posts style updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
   # Index action to render all posts
   def index
-    @posts = Post.all
+    @posts = Post.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 8)
   end
 
   # New action for creating post
