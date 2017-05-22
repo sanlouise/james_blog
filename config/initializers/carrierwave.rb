0198e7b0ef80fcffc409e002455ccffdedcbdc47
sanlouise <sandra.adamshallie@gmail.com>
2017-05-22 09:19:40 -0400

    Updates for fog in production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,18 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      :provider => 'AWS',
-      :aws_access_key_id => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :region => 'eu-central-1'
-    }
-    
-    config.fog_directory = ENV['S3_BUCKET']
-  end
-  
+CarrierWave.configure do |config|
+
+  config.storage = :fog
+
+  config.fog_credentials = {
+    :provider              => 'AWS',
+    :aws_access_key_id     => AWS_ACCESS_KEY_ID,
+    :aws_secret_access_key => AWS_SECRET_ACCESS_KEY,
+    :region                => 'us-east-1'
+  }
+
+  config.asset_host     = CDN_HOST
+  config.fog_directory  = S3_BUCKET_NAME
+  config.fog_public     = true
+  config.fog_attributes = {
+    'Cache-Control' => "max-age=#{1.year.to_i}"
+  }
 end
